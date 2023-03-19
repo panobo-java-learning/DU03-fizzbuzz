237956aebf3881ac23834e4de70e4aa488cd9c2b
Pavel Svejda <pavlis.1@seznam.cz>
2023-03-19 17:44:00 +0100

    Add comments

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,13 @@ public class Main {
         }
     }
 
+    /**
+     * Read number and according to remaining after int division return "Fizz" (number % 3 == 0),
+     *   "Buzz" (number % 5 == 0), "FizzBuzz" (previous both at the same time), or
+     *   empty String if none above applies.
+     * @param number [int]
+     * @return [String]
+     * */
     public static String markFizzBuzz(int number) {
         String result = "";
         if (number % 3 == 0) { result += "Fizz"; }
